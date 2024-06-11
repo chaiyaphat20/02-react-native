bec43767dbd13359aacd08deecfdecb205a147f0
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2024-06-11 17:49:55 +0700

    init: add style

--- App.tsx
+++ App.tsx
@@ -1,49 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableHighlight,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
+import { QUESTION } from './src/constants';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const [page, setPage] = useState(1)
+  const question = QUESTION[page - 1]
   return (
-    <SafeAreaView>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View>
-          <Text style={styles.highlight}>Hello</Text>
+    <SafeAreaView style={{ padding: 10, }}>
+      <View style={{ display: 'flex', flexDirection: 'row', position: 'static', justifyContent: 'space-between', alignItems: 'flex-start', width: '100%' }}>
+        <Text style={{ fontSize: 18 }}>{page} of 20</Text>
+        <Text style={{ fontSize: 18 }}>Time</Text>
+      </View>
+      <ScrollView>
+        <View style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'white', flex: 1, alignItems: 'center' }}>
+          <View style={{ marginTop: 20, marginBottom: 30, width: '100%', gap: 14 }}>
+            <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', width: '100%', marginTop: 10 }}>
+              <View >
+                <Text style={styles.title}>Question {page}</Text>
+              </View>
+              <View style={{ marginTop: 10 }}>
+                <Text style={styles.detail}>{question.question}</Text>
+              </View>
+            </View>
+
+            <TouchableHighlight
+              style={styles.btnChoice}
+              onPress={() => console.log('Left button pressed')}
+              underlayColor="lightgray"
+            >
+              <Text style={styles.textChoice}>A. {question.listQuestion[0].answer}</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.btnChoice}
+              onPress={() => console.log('Left button pressed')}
+              underlayColor="lightgray"
+            >
+              <Text style={styles.textChoice}>B. {question.listQuestion[1].answer}</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.btnChoice}
+              onPress={() => console.log('Left button pressed')}
+              underlayColor="lightgray"
+            >
+              <Text style={styles.textChoice}>C. {question.listQuestion[2].answer}</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.btnChoice}
+              onPress={() => console.log('Left button pressed')}
+              underlayColor="lightgray"
+            >
+              <Text style={styles.textChoice}>D. {question.listQuestion[3].answer}</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={{ flexDirection: 'row', display: 'flex', gap: 10 }}>
+            <Button title='PREV' onPress={() => {
+              if (page > 1) {
+                setPage(prev => prev - 1)
+              }
+            }} />
+            <Button title='NEXT' onPress={() => {
+              if (page < QUESTION.length) {
+                setPage(prev => prev + 1)
+              }
+            }} />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -51,8 +80,29 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  highlight :{
-    color:'red'
+  headerSection: {
+    fontSize: 20
+  },
+  title: {
+    color: 'black',
+    fontWeight: '900',
+    fontSize: 24
+  },
+  detail: {
+    color: 'black',
+    fontWeight: '900',
+    fontSize: 24
+  },
+  btnChoice: {
+    padding: 20,
+    borderColor: 'gray',
+    borderWidth: 2,
+    borderRadius: 10
+  },
+  textChoice: {
+    color: 'black',
+    fontWeight: '600',
+    fontSize: 20,
   }
 });
 
---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const QUESTION = [
+  {
+    id: 1,
+    question: "1 + 1 = ?",
+    listQuestion: [{ answer: "1", correct: false }, { answer: "2", correct: true }, { answer: "3", correct: false }, { answer: "4", correct: false }]
+  },
+  {
+    id: 1,
+    question: "13+ 1 = ?",
+    listQuestion: [{ answer: "14", correct: true }, { answer: "24", correct: false }, { answer: "3", correct: false }, { answer: "4", correct: false }]
+  },
+]
